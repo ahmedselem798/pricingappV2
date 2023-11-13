99242330a96fcd008d3e374161dc130e15b64f0e
Salma Osama <sim.salmaosama3056@alexu.edu.eg>
2023-11-13 19:01:17 +0200

    tazabeet

---
+++ client/src/Components/CountriesTable/CountriesTable.js
@@ -0,0 +1,45 @@
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// const CountriesTable = () => {
+//   return (
+//     <table className="table table-striped-columns">
+//       <thead>
+//         <tr>
+//           <th>Country</th>
+//           <th>Networks</th>
+//           <th>VPMN</th>
+//           <th>Data Cost</th>
+//           <th>IMSI</th>
+//           <th>Provider</th>
+//           <th>2G</th>
+//           <th>3G</th>
+//           <th>LTE</th>
+//           <th>5G</th>
+//           <th>LTE_M</th>
+//           <th>NB_IoT</th>
+//           <th>Notes</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         <tr>
+//           <td>Egypt</td>
+//           <td>Vodafone</td>
+//           <td>Vod</td>
+//           <td>0.0322$</td>
+//           <td>22</td>
+//           <td>PondMobile</td>
+//           <td>yes</td>
+//           <td>yes</td>
+//           <td>yes</td>
+//           <td>no</td>
+//           <td>no</td>
+//           <td>no</td>
+//           <td>No notes yet</td>
+//         </tr>
+//       </tbody>
+//     </table>
+//   );
+// };
+
+// export default CountriesTable;
