4a23af686c3c4c146a4d3086382d2ee11a3c533e
Salma Osama <sim.salmaosama3056@alexu.edu.eg>
2023-11-13 18:08:00 +0200

    CountryFormUpdate

--- client/src/Components/CountryForm/CountryForm.js
+++ client/src/Components/CountryForm/CountryForm.js
@@ -1,95 +1,25 @@
 import React, { useState } from 'react';
-import './CountryForm.css'; 
+import './CountryForm.css';
 import { Form, Container, Row, Col, Button, InputGroup } from 'react-bootstrap';
-
-const countryOptions = [
-  { value: 'afghanistan', label: 'Afghanistan' },
-  { value: 'aland-islands', label: 'Aland Islands' },
-  { value: 'albania', label: 'Albania' },
-  { value: 'algeria', label: 'Algeria' },
-  { value: 'andorra', label: 'Andorra' },
-  { value: 'angola', label: 'Angola' },
-  { value: 'anguilla', label: 'Anguilla' },
-  { value: 'antigua-and-barbuda', label: 'Antigua and Barbuda' },
-  { value: 'argentina', label: 'Argentina' },
-  { value: 'armenia', label: 'Armenia' },
-  { value: 'aruba', label: 'Aruba' },
-  { value: 'australia', label: 'Australia' },
-  { value: 'austria', label: 'Austria' },
-  { value: 'bahrain', label: 'Bahrain' },
-  { value: 'canada', label: 'Canada' },
-  { value: 'france', label: 'France' },
-  { value: 'germany', label: 'Germany' },
-  { value: 'kuwait', label: 'Kuwait' },
-  { value: 'nigeria', label: 'Nigeria' },
-  { value: 'oman', label: 'Oman' },
-  { value: 'philippines', label: 'Philippines' },
-  { value: 'qatar', label: 'Qatar' },
-  { value: 'saudi', label: 'Saudi Arabia' },
-  { value: 'singapore', label: 'Singapore' },
-  { value: 'turkey', label: 'Turkey' },
-  { value: 'uae', label: 'UAE' },
-  { value: 'unitedkingdom', label: 'United Kingdom' },
-  { value: 'unitedstates', label: 'United States' },
-];
 
 function CountryForm() {
   const [networks, setNetworks] = useState(['']);
-  const [vpmnValue, setvpmnValue] = useState(['']);
+  const [vpmnValue, setVpmnValue] = useState(['']);
   const [dataCosts, setDataCosts] = useState(['']);
   const [imsi, setImsi] = useState(['']);
   const [provider, setProvider] = useState(['']);
+  const [notes, setNotes] = useState('');
   const [formData, setFormData] = useState({
     countryName: '',
-    technologies: [],
+    technologies: [
+      { label: '2G', value: '' },
+      { label: '3G', value: '' },
+      { label: 'LTE', value: '' },
+      { label: '5G', value: '' },
+      { label: 'LTE-M', value: '' },
+      { label: 'NB_IoT', value: '' },
+    ],
   });
-
-  const handleNetworkChange = (index, value) => {
-    const updatedNetworks = [...networks];
-    updatedNetworks[index] = value;
-    setNetworks(updatedNetworks);
-  };
-
-  const handleDataCostChange = (index, value) => {
-    const updatedDataCosts = [...dataCosts];
-    updatedDataCosts[index] = value;
-    setDataCosts(updatedDataCosts);
-  };
-
-  const handleImsiChange = (index, value) => {
-    const updatedImsi = [...imsi];
-    updatedImsi[index] = value;
-    setImsi(updatedImsi);
-  };
-
-  const handlevpmnChange = (index, value) => {
-    const updatedvpmnValue = [...vpmnValue];
-    updatedvpmnValue[index] = value;
-    setvpmnValue(updatedvpmnValue);
-  };
-
-  const handleProviderChange = (index, value) => {
-    const updatedProvider = [...provider];
-    updatedProvider[index] = value;
-    setProvider(updatedProvider);
-  };
-
-  // const handleAddNetwork = () => {
-  //   setNetworks([...networks, '']);
-  //   setDataCosts([...dataCosts, '']);
-  //   setImsi([...imsi, '']);
-  //   setvpmnValue([...vpmnValue, '']);
-  // };
-
-  const handleTechnologyChange = (technology) => {
-    const updatedTechnologies = formData.technologies.includes(technology)
-      ? formData.technologies.filter((tech) => tech !== technology)
-      : [...formData.technologies, technology];
-    setFormData({
-      ...formData,
-      technologies: updatedTechnologies,
-    });
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -103,113 +33,100 @@ function CountryForm() {
           <Col>
             <Form onSubmit={handleSubmit}>
               <Form.Group>
-                <Form.Label>Country</Form.Label>
-                <Form.Control
-                  as="select"
-                  value={formData.countryName}
-                  onChange={(e) => setFormData({ ...formData, countryName: e.target.value })}
-                >
-                  <option value="" disabled hidden>Select a country</option>
-                  {countryOptions.map((country) => (
-                    <option key={country.value} value={country.value}>
-                      {country.label}
-                    </option>
-                  ))}
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Group>
-                <Form.Label>Networks</Form.Label>
-                {networks.map((network, index) => (
-                  <InputGroup key={index} className="mb-2">
+                <Row>
+                  <Col>
+                    <Form.Label>Country</Form.Label>
                     <Form.Control
                       type="text"
-                      value={network}
-                      onChange={(e) => handleNetworkChange(index, e.target.value)}
+                      value={formData.countryName}
+                      onChange={(e) => setFormData({ ...formData, countryName: e.target.value })}
                     />
-                  </InputGroup>
-                ))}
-                <Button type="button" variant="secondary" size="lg"
-                //  onClick={handleAddNetwork}
-                 >
-                  Add
-                </Button>
-                
-                <Button type="button" variant="third" size="lg"
-                //  onClick={handleAddNetwork}
-                 >
-                  Edit
-                </Button>
-              </Form.Group>
-              
-
-              <Form.Group>
-                <Form.Label>VPMN</Form.Label>
-                {vpmnValue.map((vpmnValue, index) => (
-                  <InputGroup key={index} className="mb-2">
-                    <Form.Control
-                      type="text"
-                      value={vpmnValue}
-                      onChange={(e) => handlevpmnChange(index, e.target.value)}
-                    />
-                  </InputGroup>
-                ))}
+                  </Col>
+                  <Col>
+                    <Form.Label>Networks</Form.Label>
+                    {networks.map((network, index) => (
+                      <InputGroup key={index} className="mb-2">
+                        <Form.Control type="text" value={network} />
+                      </InputGroup>
+                    ))}
+                  </Col>
+                </Row>
               </Form.Group>
 
               <Form.Group>
-                <Form.Label>Data Cost</Form.Label>
-                {dataCosts.map((dataCost, index) => (
-                  <InputGroup key={index} className="mb-2">
-                    <Form.Control
-                      type="text"
-                      value={dataCost}
-                      onChange={(e) => handleDataCostChange(index, e.target.value)}
-                    />
-                  </InputGroup>
-                ))}
+                <Row>
+                  <Col>
+                    <Form.Label>VPMN</Form.Label>
+                    {vpmnValue.map((vpmn, index) => (
+                      <InputGroup key={index} className="mb-2">
+                        <Form.Control type="text" value={vpmn} />
+                      </InputGroup>
+                    ))}
+                  </Col>
+                  <Col>
+                    <Form.Label>Data Cost</Form.Label>
+                    {dataCosts.map((dataCost, index) => (
+                      <InputGroup key={index} className="mb-2">
+                        <Form.Control type="text" value={dataCost} />
+                      </InputGroup>
+                    ))}
+                  </Col>
+                </Row>
               </Form.Group>
 
               <Form.Group>
-                <Form.Label>IMSI</Form.Label>
-                {imsi.map((imsiValue, index) => (
-                  <InputGroup key={index} className="mb-2">
-                    <Form.Control
-                      type="number"
-                      value={imsiValue}
-                      onChange={(e) => handleImsiChange(index, e.target.value)}
-                    />
-                  </InputGroup>
-                ))}
-              </Form.Group>
-
-              <Form.Group>
-                <Form.Label>Provider</Form.Label>
-                {provider.map((provider, index) => (
-                  <InputGroup key={index} className="mb-2">
-                    <Form.Control
-                      type="text"
-                      value={provider}
-                      onChange={(e) => handleProviderChange(index, e.target.value)}
-                    />
-                  </InputGroup>
-                ))}
+                <Row>
+                  <Col>
+                    <Form.Label>IMSI</Form.Label>
+                    {imsi.map((imsiValue, index) => (
+                      <InputGroup key={index} className="mb-2">
+                        <Form.Control type="number" value={imsiValue} />
+                      </InputGroup>
+                    ))}
+                  </Col>
+                  <Col>
+                    <Form.Label>Provider</Form.Label>
+                    {provider.map((providerValue, index) => (
+                      <InputGroup key={index} className="mb-2">
+                        <Form.Control type="text" value={providerValue} />
+                      </InputGroup>
+                    ))}
+                  </Col>
+                </Row>
               </Form.Group>
 
               <h2 className='h1'>Technologies</h2>
 
-              {['2G', '3G', 'LTE', '5G', 'LTE-M', 'NB_IoT'].map((technology) => (
-                <Form.Check
-                  key={technology}
-                  type="checkbox"
-                  label={technology}
-                  checked={formData.technologies.includes(technology)}
-                  onChange={() => handleTechnologyChange(technology)}
-                />
-              ))}
+              <Row>
+                {formData.technologies.map((technology, index) => (
+                  <Col key={index} md={6}>
+                    <InputGroup className="mb-2">
+                      <Form.Control
+                        type="text"
+                        placeholder={technology.label}
+                        value={technology.value}
+                        onChange={(e) => {
+                          const updatedTechnologies = [...formData.technologies];
+                          updatedTechnologies[index].value = e.target.value;
+                          setFormData({
+                            ...formData,
+                            technologies: updatedTechnologies,
+                          });
+                        }}
+                      />
+                    </InputGroup>
+                  </Col>
+                ))}
+              </Row>
 
-              <Button type="submit" variant="primary" size="lg">
+              <Form.Group>
+                <Form.Label className='notes'>Notes</Form.Label>
+                <Form.Control as="textarea" rows={3} value={notes} />
+              </Form.Group>
+
+              <button type="button" className="btn">
                 Submit
-              </Button>
+              </button>
             </Form>
           </Col>
         </Row>
@@ -219,3 +136,4 @@ function CountryForm() {
 }
 
 export default CountryForm;
+
