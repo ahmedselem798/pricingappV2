0aea2fc7e9d5339663625812c57ac25db2bfe0e1
Salma Osama <sim.salmaosama3056@alexu.edu.eg>
2023-11-16 14:05:59 +0200

    done with countries table edits

--- client/src/Components/CountriesTable/CountriesTable.js
+++ client/src/Components/CountriesTable/CountriesTable.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./CountriesTable.css";
@@ -5,12 +6,16 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const CountriesTable = () => {
-  const [Country, SetCountry] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [sortOrder, setSortOrder] = useState({
+    column: "countryName",
+    direction: "asc",
+  });
 
   useEffect(() => {
     axios
       .get("http://localhost:5000/")
-      .then((result) => SetCountry(result.data))
+      .then((result) => setCountries(result.data))
       .catch((err) => console.log(err));
   }, []);
 
@@ -24,15 +29,43 @@ const CountriesTable = () => {
       .catch((err) => console.log(err));
   };
 
+  const handleSort = () => {
+    setSortOrder((prevSortOrder) => ({
+      column: "countryName",
+      direction:
+        prevSortOrder.column === "countryName" && prevSortOrder.direction === "asc"
+          ? "desc"
+          : "asc",
+    }));
+  };
+
+  const sortedCountries = [...countries].sort((a, b) => {
+    const isDesc = sortOrder.direction === "desc" ? -1 : 1;
+    const countryA = a[sortOrder.column].toLowerCase();
+    const countryB = b[sortOrder.column].toLowerCase();
+
+    if (countryA < countryB) {
+      return -1 * isDesc;
+    }
+    if (countryA > countryB) {
+      return 1 * isDesc;
+    }
+    return 0;
+  });
+
   return (
     <div>
-      <Link to="/create" className="btn custom-btn btn-success mb-3">
-        Add Country
+      <Link to="/create">
+        <button type="button" className="btn custom-btn btn-success mb-3">
+          Add Country
+        </button>
       </Link>
       <table className="table table-striped-columns">
-        <thead>
+        <thead className="text-center">
           <tr>
-            <th>Country</th>
+            <th onClick={handleSort} className="country-header">
+              Country
+            </th>
             <th>Networks</th>
             <th>VPMN</th>
             <th>IMSI</th>
@@ -50,33 +83,39 @@ const CountriesTable = () => {
             <th>Actions</th>
           </tr>
         </thead>
-        <tbody>
-          {Country.map((Country) => {
+        <tbody className="text-center">
+          {sortedCountries.map((country) => {
             return (
-              <tr>
-                <td>{Country.countryName}</td>
-                <td>{Country.network}</td>
-                <td>{Country.vpmn}</td>
-                <td>{Country.imsi}</td>
-                <td>{Country.dataCostPerMB}</td>
-                <td>{Country.dataCostPerMB * 1024}</td>
-                <td>{Country.provider}</td>
-                <td>{Country._2G}</td>
-                <td>{Country._3G}</td>
-                <td>{Country._4G}</td>
-                <td>{Country._5G}</td>
-                <td>{Country.lte}</td>
-                <td>{Country.lte_m}</td>
-                <td>{Country.nb_iot}</td>
-                <td>{Country.note}</td>
+              <tr key={country._id}>
+                <td>{country.countryName}</td>
+                <td>{country.network}</td>
+                <td>{country.vpmn}</td>
+                <td>{country.imsi}</td>
+                <td>{country.dataCostPerMB}</td>
+                <td>{country.dataCostPerMB * 1024}</td>
+                <td>{country.provider}</td>
+                <td>{country._2G}</td>
+                <td>{country._3G}</td>
+                <td>{country._4G}</td>
+                <td>{country._5G}</td>
+                <td>{country.lte}</td>
+                <td>{country.lte_m}</td>
+                <td>{country.nb_iot}</td>
+                <td>{country.note}</td>
                 <td>
-                  <button
-                    onClick={(e) => handleDelete(Country._id)}
-                    className="btn custom-btn btn-edit mr-2"
-                  >
-                    Delete
-                  </button>
-                  <Link to={`/update/${Country._id}`} className="btn custom-btn btn-add">Edit</Link>
+                  <div className="btn-container">
+                    <button
+                      onClick={(e) => handleDelete(country._id)}
+                      className="btn custom-btn btn-danger mr-2">
+                      Delete
+                    </button>
+
+                    <Link to={`/update/${country._id}`}>
+                      <button type="button" className="btn btn-info">
+                        Edit
+                      </button>
+                    </Link>
+                  </div>
                 </td>
               </tr>
             );
@@ -88,3 +127,4 @@ const CountriesTable = () => {
 };
 
 export default CountriesTable;
+
