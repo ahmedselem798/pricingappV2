8c5be85f8344c7c1243bf75a338afea686a17b55
salmaosama77 <salma@deplofy.com>
2023-12-14 20:20:48 +0200

    ..

--- client/src/Components/sortData.js
+++ client/src/Components/sortData.js
@@ -1,29 +1,29 @@
-export const sortData = (data, sortOrder) => {
-    const { column, direction } = sortOrder;
+// export const sortData = (data, sortOrder) => {
+//     const { column, direction } = sortOrder;
   
-    const compareFunction = (a, b) => {
-      const valueA = a[column].toLowerCase();
-      const valueB = b[column].toLowerCase(); 
+//     const compareFunction = (a, b) => {
+//       const valueA = a[column].toLowerCase();
+//       const valueB = b[column].toLowerCase(); 
   
-      if (column === "countryName") {
-        // Sort by countryName alphabetically
-        const countryComparison = valueA.localeCompare(valueB);
-        if (countryComparison !== 0) {
-          return direction === "asc" ? countryComparison : -countryComparison;
-        }
-      }
+//       if (column === "countryName") {
+//         // Sort by countryName alphabetically
+//         const countryComparison = valueA.localeCompare(valueB);
+//         if (countryComparison !== 0) {
+//           return direction === "asc" ? countryComparison : -countryComparison;
+//         }
+//       }
   
-      // If country names are equal or sorting by a different column, proceed to the next criteria
-      // Sort by provider alphabetically
-      const providerComparison = a.provider.toLowerCase().localeCompare(b.provider.toLowerCase());
-      if (providerComparison !== 0) {
-        return direction === "asc" ? providerComparison : -providerComparison;
-      }
+//       // If country names are equal or sorting by a different column, proceed to the next criteria
+//       // Sort by provider alphabetically
+//       const providerComparison = a.provider.toLowerCase().localeCompare(b.provider.toLowerCase());
+//       if (providerComparison !== 0) {
+//         return direction === "asc" ? providerComparison : -providerComparison;
+//       }
   
-      // If providers are also equal, sort by IMSI numbers
-      return direction === "asc" ? a.imsi - b.imsi : b.imsi - a.imsi;
-    };
+//       // If providers are also equal, sort by IMSI numbers
+//       return direction === "asc" ? a.imsi - b.imsi : b.imsi - a.imsi;
+//     };
   
-    return [...data].sort(compareFunction);
-  };
+//     return [...data].sort(compareFunction);
+//   };
   
